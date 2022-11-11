8ac9f7c0e72ce67b89bf921bbdcd5eb71e44d166
Oliver Guo <oliguo@users.noreply.github.com>
2022-11-11 16:54:22 +0800

    Update entry.sh
    add mssql support

--- docker/alpine-apache-php7/entry.sh
+++ docker/alpine-apache-php7/entry.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+echo extension=pdo_sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/10_pdo_sqlsrv.ini
+echo extension=sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/20_sqlsrv.ini
+
 umask 0000
 ##Start crond
 /usr/sbin/crond start
