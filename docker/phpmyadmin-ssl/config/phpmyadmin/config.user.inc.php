5bc5c92a0f593792304695c26a3572f67c8b9970
Oliver Guo <oliguo@users.noreply.github.com>
2023-09-23 11:50:14 +0800

    Create config.user.inc.php

---
+++ docker/phpmyadmin-ssl/config/phpmyadmin/config.user.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$i=0;
+$i++;
+$cfg['Servers'][$i]['auth_type']     = 'cookie';
+// if you insist on "root" having no password:
+//$cfg['Servers'][$i]['AllowNoPassword'] = true; `
+
+//$cfg['Servers'][$i]['user']          = 'root';
+//$cfg['Servers'][$i]['password']      = 'root'; // use here your password
+//$cfg['Servers'][$i]['auth_type']     = 'config';
